8269eb6c8f7f18ccef33868ba18eace4614d51bd
Kevin-Detournay <kevindetournay@hotmail.fr>
2021-03-02 16:40:04 +0100

    building the structure of app +making the mobile menu + fetching articles and categories

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -7,7 +7,7 @@ import cover from 'src/assets/images/cover.jpg';
 const Home = () => (
   <Page>
     <div className="cover">
-      <img src={cover} alt=""/>
+      <img src="https://images.pexels.com/photos/6147251/pexels-photo-6147251.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" alt=""/>
     </div>
     <Content />
   </Page>
