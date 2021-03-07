b27dafb332213aa8baf66b13833c0bde751d6330
Florent-Gorski <florentgorski974@gmail.com>
2021-03-08 03:03:03 +0400

    add avatars + end of page about with Scss

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -2,53 +2,96 @@
 import React from 'react';
 
 import Page from 'src/components/Page';
+
+import imgFlorent from 'src/assets/images/Florent.png';
+import imgKevin from 'src/assets/images/Kevin.png';
+import imgMagomed from 'src/assets/images/Magomed.png';
+import imgVincent from 'src/assets/images/Vincent.png';
+
 import './style.scss';
 
 const About = () => (
-  <Page>
-    <div className="container">
-      <div className="row">
-        <div className="col-md-4">
-          <div className="panel widget">
-            <div className="half-float">
-              <img src="https://bootdey.com/img/Content/bg1.jpg" alt="" className="img-responsive" />
-              <div className="half-float-bottom">
-                <img
-                  src="https://bootdey.com/img/Content/avatar/avatar7.png"
-                  alt="Image"
-                  className="img-thumbnail img-circle thumb128"
-                />
-              </div>
-              <div className="panel-body text-center">
-                <h3 className="m0">Clark Kentman</h3>
-                <p className="text-muted">Lead director</p>
-                <p>Proin metus justo, commodo in ultrices at,
-                  lobortis sit amet dui. Fusce dolor purus, adipiscing a tempus
-                  at, gravida vel purus.
-                </p>
-              </div>
-              <div className="panel-body text-center bg-gray-dark">
-                <div className="row row-table">
-                  <div className="col-xs-4">
-                    <h3 className="m0">400</h3>
-                    <p className="m0">Photos</p>
-                  </div>
-                  <div className="col-xs-4">
-                    <h3 className="m0">2000</h3>
-                    <p className="m0">Likes</p>
-                  </div>
-                  <div className="col-xs-4">
-                    <h3 className="m0">500</h3>
-                    <p className="m0">Following</p>
-                  </div>
-                </div>
-              </div>
+<Page>
+  <h1 className="about_title">À propos de nous</h1>
+  <div className="container">
+    <div className="d-flex">
+      <div className="card_profil">
+        <div className="boxs project_widget">
+          <div className="pw_img">
+            <img className="img-responsive" src={imgKevin} alt="avatar" />
+          </div>
+          <div className="pw_content">
+            <div className="pw_header">
+              <h6 className="card-title">
+                Kevin Detournay
+              </h6>
+              <small className="text-muted">
+                Lead-dev-front & Product Owner
+              </small>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="card_profil">
+        <div className="boxs project_widget">
+          <div className="pw_img">
+            <img className="img-responsive" src={imgMagomed} alt="avatar" />
+          </div>
+          <div className="pw_content">
+            <div className="pw_header">
+              <h6 className="card-title">
+                Magomed Voraev
+              </h6>
+              <small className="text-muted">
+                Scrum Master
+              </small>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="card_profil">
+        <div className="boxs project_widget">
+          <div className="pw_img">
+            <img className="img-responsive" src={imgVincent} alt="avatar" />
+          </div>
+          <div className="pw_content">
+            <div className="pw_header">
+              <h6 className="card-title">
+                Vincent Giglio</h6>
+              <small className="text-muted">
+                Lead-dev-back
+              </small>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="card_profil">
+        <div className="boxs project_widget">
+          <div className="pw_img">
+            <img className="img-responsive" src={imgFlorent} alt="avatar" />
+          </div>
+          <div className="pw_content">
+            <div className="pw_header">
+              <h6 className="card-title">
+                Florent Gorski
+              </h6>
+              <small className="text-muted">
+                Git Master
+              </small>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </Page>
+  </div>
+  <p className="card_text">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    Sed mattis nulla vitae diam cursus vestibulum. Sed ornare purus odio,
+    in molestie lectus mollis quis. Duis pretium tortor ipsum, sed ultricies augue imperdiet quis.
+    Nulla bibendum placerat sollicitudin. Orci varius natoque penatibus et magnis
+    dis parturient montes, nascetur ridiculus mus. Aenean cursus ex nec dignissim tempus.
+  </p>
+</Page>
 );
 
 export default About;
