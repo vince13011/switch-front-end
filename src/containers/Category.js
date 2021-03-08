4838856c3d2fa8763343764839654bfa74652dbf
Kevin-Detournay <kevindetournay@hotmail.fr>
2021-03-08 20:19:56 +0100

    filterbycategory ok

--- src/containers/Category.js
+++ src/containers/Category.js
@@ -1,14 +1,15 @@
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import Category from 'src/components/Category';
 import { findArticlesByCategory } from '../selectors/Category';
 // branchement en lecture du state
 const mapStateToProps = (state, ownProps) => {
   const { name } = ownProps.match.params;
-  const article = findArticlesByCategory(state.articles, name);
-  console.log(article);
+  const articles = findArticlesByCategory(state.articles, name);
+  
 
   return {
-    article,
+    articles,
     size: state.article.size,
   };
 };
@@ -17,5 +18,5 @@ const mapStateToProps = (state, ownProps) => {
 const mapDispatchToProps = (dispatch) => ({
 
 });
-
-export default connect(mapStateToProps, mapDispatchToProps)(Category);
+const connected = connect(mapStateToProps, mapDispatchToProps)(Category);
+export default withRouter(connected);
