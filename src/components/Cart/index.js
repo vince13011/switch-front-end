ee08f60c8386ab96fd44f3a7522e820a90cbca93
Kevin-Detournay <kevindetournay@hotmail.fr>
2021-03-10 19:29:06 +0100

    adding buttons for qtys

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import {
+  MdAdd,
+  MdRemove,
+
+} from 'react-icons/md';
 import Page from 'src/components/Page';
 import './style.scss';
 
@@ -6,20 +11,28 @@ const CART = ({ articles }) => (
   <Page>
     <div className="cart__container">
       {articles ? (
+
         articles.map((article) => (
           <div className="cart__article">
             <div className="cart__article__image"> <img src={article.image} alt="" /></div>
             <div className="cart__article__description">
-              <div className="cart__article__item--title">{article.name}</div>
-              <div className="cart__article__item--size">{article.size}</div>
-              <div className="cart__article__item--qty">quantité {article.qty}</div>
-              <div className="cart__article__item--price">{article.pre_tax_price} € </div>
+              <div className="cart__article__item cart__article__item--title">{article.name}</div>
+              <div className="cart__article__item cart__article__item--size">Taille: {article.size}</div>
+              <div className="cart__article__item cart__article__item--qty">
+                quantité: {article.qty}
+                <div>
+                  <MdRemove className="cart__article__item__qty-btn" />
+                  <MdAdd className="cart__article__item__qty-btn" />
+                </div>
+              </div>
+              <div className="cart__article__item cart__article__item--price">{article.pre_tax_price} € </div>
             </div>
-
           </div>
-        )))
+        ))
+      )
+
         : (
-          <div>Panier vide</div>
+          <h1>Panier vide</h1>
         )}
     </div>
 
