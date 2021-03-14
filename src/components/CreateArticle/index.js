e6e2c01a7a5c299d7d7c3f7c09061b23772e0b22
Kevin-Detournay <kevindetournay@hotmail.fr>
2021-03-14 21:38:14 +0100

    uploadingok

--- src/components/CreateArticle/index.js
+++ src/components/CreateArticle/index.js
@@ -1,20 +1,41 @@
 import React from 'react';
+import { app } from '../../base';
 import './style.scss';
 
-const CreateArticle = () => (
-  <>
-    <h2>Creer un nouvel Article </h2>
-    <form action="">
-      <input
-        type="text"
-        placeholder="titre"
-      />
-      <input
-        type="file"
-        placeholder="titre"
-      />
-    </form>
-  </>
-);
+const CreateArticle = () => {
+  const onFileChange = (e) => {
+    const file = e.target.files[0];
+    const storageRef = app.storage().ref();
+    const fileRef = storageRef.child(file.name);
+    fileRef.put(file).then(() => {
+      console.log('uploaded', file.name);
+    });
+  };
+
+  const onClick = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <>
+      <h2>Creer un nouvel Article </h2>
+      <form action="">
+        <input
+          type="text"
+          placeholder="titre"
+        />
+        <input
+          type="file"
+          onChange={onFileChange}
+
+        />
+        <button
+          type="submit"
+          onClick={onClick}
+        >Upload de l'image
+        </button>
+      </form>
+    </>
+  );
+};
 
 export default CreateArticle;
