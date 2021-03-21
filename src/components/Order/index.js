111ec1aaf06ea5dd8de72281880063f9351b1f9e
MagomedVoraev <voraevmagomed@gmail.com>
2021-03-21 22:49:34 +0100

    page order designing is doneé

--- src/components/Order/index.js
+++ src/components/Order/index.js
@@ -40,98 +40,94 @@ const Order = ({
   return (
     <Page>
       {order
-    && (
-    <div className="order__maincontainer">
-      <div className="order__container">
-        <h1>Récapitulatif</h1>
+        && (
+          <div className="order__maincontainer">
+            <div className="order__container">
+              <h1>Récapitulatif</h1>
 
-        <div className="order__articles">
-          <h2 className="order__subtitle">commande N° {order.order_number} </h2>
+              <div className="order__articles">
+                <h2 className="order__subtitle">Commande N° {order.order_number}</h2>
 
-          {order.articles && order.articles.map((article) => (
-            <div className="order__article">
-              <div className="order__article__image"> <img src={article.article_image} alt="" /></div>
-              <div className="order__article__description">
-                <div className="order__article__item order__article__item--title">{article.article_name}</div>
-                <div className="order__article__item order__article__item--size">Taille: {article.sizes.size}</div>
-                <div className="order__article__item order__article__item--qty">
-                  quantité: {article.sizes.quantity}
-                  <div />
-                </div>
-                <div className="order__article__item order__article__item--price">{article.unit_net_price} € </div>
+                {order.articles && order.articles.map((article) => (
+                  <div div className="order__article">
+                    <div className="order__article__image"> <img src={article.article_image} alt="" /></div>
+                    <div className="order__article__description">
+                      <div className="order__article__item order__article__item--size">Taille: {article.sizes.size}</div>
+                      <div className="order__article__item order__article__item--qty">
+                        Quantité: {article.sizes.quantity}
+                        <div />
+                      </div>
+                      <div className="order__article__item order__article__item--price">Prix {article.unit_net_price} €</div>
+                    </div>
+                  </div>
+                ))}
+
               </div>
+              <div className="order__footer">
+
+                <div className="order__footer__total">Total: {order.total_price} €</div>
+                {address && (
+                  <div className="order__footer__address">
+                    <h2 className="order__footer__subtitle">Adresse de livraison</h2>
+                    {/* <p className="order__footer__address__item" /> */}
+                    <p className="order__footer__address__item">{address.number} {address.street_name}</p>
+                    <p className="order__footer__address__item">{address.zip_code} {address.city}</p>
+                  </div>
+                )}
+
+                {/* <div className="order__footer__status"> */}
+                <div className="order__footer__status__current">Statut: {order.status_name}</div>
+                {/* </div> */}
+                {order.tracking_number && (
+                  <div className="order__footer__status">N° de suivi : {order.tracking_number}</div>
+                )}
+              </div>
+
+              {admin && (
+                <form action="" className="order__form">
+
+                  <div className="order__select--container">
+                    <label className="order__select--container-select" htmlFor="status_select"></label>
+                    <select
+                      className="order__select--container-select-status"
+                      id="status_select"
+                      name="modifiedStatus"
+                      onChange={handleChangeField}
+                    >
+                      <option value="">Choisir</option>
+                      {status
+                        && status.map(
+                          (stat) => (
+                            <option value={stat.status_name}>
+                              {stat.status_name}
+                            </option>
+                          ),
+                        )}
+                    </select>
+
+                  </div>
+                  <div className="order__select--container__tracking-number">
+                    <input
+                      className="order__select--container-input"
+                      type="text"
+                      name="modifiedTracking"
+                      placeholder="Numéro de suivi colis"
+                      onChange={handleChangeField}
+                      value={modifiedTracking}
+                    />
+                    <button
+                      className="order__select--container-btn"
+                      type="submit"
+                      onClick={handleSubmit}
+                    >Confirmer
+                    </button>
+                  </div>
+
+                </form>
+              )}
             </div>
-          ))}
-
-        </div>
-        <div className="order__footer">
-
-          {address && (
-          <div className="order__footer__address">
-            <h2 className="order__footer__subtitle">Adresse de livraison </h2>
-            <p className="order__footer__address__item" />
-            <p className="order__footer__address__item">{address.number} {address.street_name}</p>
-            <p className="order__footer__address__item">{address.zip_code} {address.city}</p>
           </div>
-          )}
-          <div className="order__footer__total">
-            Total: {order.total_price} €
-          </div>
-
-          <div className="order__footer__status">
-            <div className="order__footer__status__current">
-              Statut: {order.status_name}
-            </div>
-          </div>
-          {order.tracking_number && (
-          <div className="order__footer__status">
-            N° de suivi : {order.tracking_number}
-          </div>
-          )}
-        </div>
-
-        {admin && (
-        <form
-          action=""
-          className="order__form"
-        >
-
-          <div className="order__select--container">
-
-            <label htmlFor="status_select">Selectionner un statut</label>
-            <select
-              id="status_select"
-              name="modifiedStatus"
-              onChange={handleChangeField}
-            >
-              <option value="">-Selectionner un statut-</option>
-              {status
-          && status.map(
-            (stat) => (
-              <option value={stat.status_name}>
-                {stat.status_name}
-              </option>
-            ),
-          )}
-            </select>
-
-          </div>
-          <input
-            type="text"
-            name="modifiedTracking"
-            onChange={handleChangeField}
-            value={modifiedTracking}
-          />
-          <button
-            type="submit"
-            onClick={handleSubmit}
-          >Mettre a jour la commande
-          </button>
-        </form>
         )}
-      </div>
-    </div>
-    )}
     </Page>
   );
 };
